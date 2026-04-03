f092c006948f65289ca86bf134a1bf72a9d70a7d
Alejandra-Chavez <alejandrachavezu9@gmail.com>
2026-04-03 10:36:04 -0300

    Add files via upload

---
+++ Alejandra-Chavez ACT6/Alejandra-Chavez ACT6/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alejandra_Chavez_ACT6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { }
+            /*1. Desarrollar una clase que represente un punto en el plano y tenga los
+siguientes métodos: cargar los valores de x e y, imprimir en que cuadrante se
+encuentra dicho punto (concepto matemático, primer cuadrante si x e y son
+positivas, si x < ;0 e y > 0 segundo cuadrante, etc.)*/
+
+            class Punto
+        {
+            private int x;
+            private int y;
+            string linea;
+            public void CargarValores()
+            {
+                Console.Write("Ingrese valor de x: ");
+                linea = Console.ReadLine();
+                x = int.Parse(linea);
+
+                Console.Write("Ingrese valor de y: ");
+                linea = Console.ReadLine();
+                y = int.Parse(linea);
+            }
+
+            public void ImprimirCuadrante()
+            {
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("Primer cuadrante");
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("Segundo cuadrante");
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("Tercer cuadrante");
+                }
+                else if (x > 0 && y < 0)
+                {
+                    Console.WriteLine("Cuarto cuadrante");
+                }
+
+            }
+
+            static void Main(string[] args)
+            {
+                Punto p = new Punto();
+                p.CargarValores();
+                p.ImprimirCuadrante();
+            }
+        }
+      }
+    }
+
---
+++ Alejandra-Chavez ACT6/Punto 4/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { }
+            /*4. Implementar la clase operaciones. Se deben cargar dos valores enteros,
+calcular su suma, resta, multiplicación y división, cada una en un método,
+imprimir dichos resultados.*/
+
+            class Operaciones
+        {
+            private int valor1;
+            private int valor2;
+            private int suma;
+            private int resta;
+            private int multiplicacion;
+            private int division;
+            string linea;
+            public void CargarValores()
+            {
+                Console.Write("Ingrese el primer valor: "); 
+                linea = Console.ReadLine();
+                valor1 = int.Parse(linea);
+
+                Console.Write("Ingrese el segundo valor: ");
+                linea += Console.ReadLine();
+                valor2 = int.Parse(linea);
+            }
+
+            
+            public void Sumar()
+            {
+                suma = valor1 + valor2;
+                Console.WriteLine("Suma: " + suma);
+            }
+
+            public void Restar()
+            {
+               resta = valor1 - valor2;
+                Console.WriteLine("Resta: " + resta);
+            }
+
+            public void Multiplicar()
+            {
+                 multiplicacion = valor1 * valor2;
+                Console.WriteLine("Multiplicación: " + multiplicacion);
+            }
+
+            public void Dividir()
+            {
+                
+                    division = valor1 / valor2;
+                Console.WriteLine("division: " + division);
+            }
+
+            static void Main(string[] args)
+            {
+                Operaciones op = new Operaciones();
+                op.CargarValores();
+                op.Sumar();
+                op.Restar();
+                op.Multiplicar();
+                op.Dividir();
+            }
+        }
+
+    }
+}
