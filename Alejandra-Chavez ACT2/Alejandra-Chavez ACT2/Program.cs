b23f032b323bf001ee95182dc38193042c6bd5f5
Alejandra-Chavez <alejandrachavezu9@gmail.com>
2026-04-06 20:54:00 -0300

    Add files via upload

---
+++ Alejandra-Chavez ACT2/Alejandra-Chavez ACT2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alejandra_Chavez_ACT2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1. Realizar un programa que lea por teclado dos números, si el primero es mayor al segundo informar su suma y diferencia,
+             * en caso contrario informar el producto y la división del primero respecto al segundo.*/
+
+            int num1, num2;
+            int suma, resta;
+            int producto, division;
+            Console.Write("Ingrese el primer número: ");
+            string linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+
+            Console.Write("Ingrese el segundo número: ");
+            linea = Console.ReadLine();
+            num2 = int.Parse(linea);
+
+            if (num1 > num2)
+            {
+                suma = num1 + num2;
+                resta = num1 - num2;
+
+                Console.WriteLine("Suma: " + suma);
+                Console.WriteLine("Diferencia: " + resta);
+            }
+            else
+            {
+                producto = num1 * num2;
+                division = num1 / num2;
+
+                Console.WriteLine("Producto: " + producto);
+                Console.WriteLine("División: " + division);
+            }
+        }
+    }
+}
+
---
+++ Alejandra-Chavez ACT2/Punto2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2. Se ingresan seis notas de un alumno, si el promedio es mayor o igual a siete mostrar un mensaje "Promocionado"*/
+
+            int n1, n2, n3, n4, n5, n6;
+
+            Console.Write("Ingrese nota 1: ");
+            string linea = Console.ReadLine();
+            n1 = int.Parse(linea);
+
+            Console.Write("Ingrese nota 2: ");
+            linea = Console.ReadLine();
+            n2 = int.Parse(linea);
+
+            Console.Write("Ingrese nota 3: ");
+            linea = Console.ReadLine();
+            n3 = int.Parse(linea);
+
+            Console.Write("Ingrese nota 4: ");
+            linea = Console.ReadLine();
+            n4 = int.Parse(linea);
+
+            Console.Write("Ingrese nota 5: ");
+            linea = Console.ReadLine();
+            n5 = int.Parse(linea);
+
+            Console.Write("Ingrese nota 6: ");
+            linea = Console.ReadLine();
+            n6 = int.Parse(linea);
+
+            int promedio = (n1 + n2 + n3 + n4 + n5 + n6) / 6;
+
+            if (promedio >= 7)
+            {
+                Console.WriteLine("Promocionado");
+            }
+        }
+    }
+}
+
+
