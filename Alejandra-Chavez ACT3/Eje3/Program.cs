f63ab87ed135be45380c7a17f755773cb2e0e6e8
Alejandra-Chavez <alejandrachavezu9@gmail.com>
2026-04-06 20:31:56 -0300

    Add files via upload

---
+++ Alejandra-Chavez ACT3/Eje3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eje3
+{
+    internal class Eje3
+    {
+        static void Main(string[] args)
+        {
+
+            // 3. Se ingresan por teclado tres números, si todos los valores ingresados son menores a 10, imprimir en pantalla la leyenda "Todos los números son menores a diez"
+            int num1, num2, num3;
+
+            Console.Write("Ingrese un numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese un numero: ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese un numero: ");
+            num3 = int.Parse(Console.ReadLine());
+
+            if (num1 < 10 && num2 < 10 && num3 < 10)
+            {
+                Console.WriteLine("todos los numeros son menores a 10");
+            }
+        }
+    }
+}
---
+++ Alejandra-Chavez ACT3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alejandra_Chavez_ACT3
+{
+    internal class Eje1
+    {
+        static void Main(string[] args)
+        {
+
+            // 1. Realizar un programa que pida cargar una fecha cualquiera, luego verificar si dicha fecha corresponde a Navidad.
+            int dia, mes;
+
+            Console.Write("Ingrese el día: ");
+            string linea = Console.ReadLine();
+            dia = int.Parse(linea);
+
+            Console.Write("Ingrese el numero de mes: ");
+             linea = Console.ReadLine();
+            mes= int.Parse(linea);
+
+            if (dia == 25 && mes == 12)
+            {
+                Console.WriteLine("ES NAVIDAD 🎄");
+            }
+            
+
+
+        }
+    }
+}
