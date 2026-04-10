340c9cf0b8a6ad51afbf9eee7713ef01e8c76ca8
Alejandra-Chavez <alejandrachavezu9@gmail.com>
2026-04-10 10:57:13 -0300

    Add files via upload

---
+++ Alejandra-Chavez ACT6/Punto 2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto_2
+{
+    internal class Cuadrado
+    {
+            /*2. Desarrollar una clase que represente un Cuadrado y tenga los siguientes
+métodos: cargar el valor de su lado, imprimir su perímetro y su superficie.*/
+
+            private int lado;
+            private int superficie;
+            private int perimetro;
+            string linea;
+
+
+            // Método para cargar el valor del lado
+            public void CargarValor()
+            {
+                Console.Write("Ingrese el valor del lado: ");
+                linea = Console.ReadLine();
+                lado = int.Parse(linea);
+            }
+
+            public void ImprimirPerimetro()
+            {
+                perimetro = lado * 4;
+                Console.WriteLine("El perímetro es: " + perimetro);
+            }
+
+            public void ImprimirSuperficie()
+            {
+                superficie = lado * lado;
+                Console.WriteLine("La superficie es: " + superficie);
+            }
+
+            static void Main(string[] args)
+            {
+                Cuadrado c = new Cuadrado();
+                c.CargarValor();
+                c.ImprimirPerimetro();
+                c.ImprimirSuperficie();
+
+            Console.ReadKey();
+            }
+        }
+    }
+    
+}
---
+++ Alejandra-Chavez ACT6/Punto 3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto_3
+{
+    internal class Empleado
+    {
+        /*3. Confeccionar una clase que represente un empleado. Definir como atributos su
+nombre y su sueldo. Confeccionar los métodos para la carga, otro para imprimir
+sus datos y por último uno que imprima un mensaje si debe pagar impuestos
+(si el sueldo supera a 3000).*/
+
+            private int nombre;
+            private int sueldo;
+            string linea;
+            public void CargarDatos()
+            {
+                Console.Write("Ingrese el nombre del empleado: ");
+                linea = Console.ReadLine();
+                nombre = int.Parse(linea);
+
+                Console.Write("Ingrese el) sueldo: ");
+                linea += Console.ReadLine();
+                sueldo = int.Parse(linea);
+            }
+
+            public void ImprimirDatos()
+            {
+                Console.WriteLine("Nombre: " + nombre);
+                Console.WriteLine("Sueldo: " + sueldo);
+            }
+
+           
+            public void PagaImpuestos()
+            {
+                if (sueldo > 3000) 
+                {
+                    Console.WriteLine("Debe pagar impuestos");
+                }
+                
+            }
+
+            static void Main(string[] args)
+            {
+                Empleado emp = new Empleado();
+                emp.CargarDatos();
+                emp.ImprimirDatos();
+                emp.PagaImpuestos();
+
+            Console.ReadKey();
+            }
+        }
+
+    }
+    
+}
