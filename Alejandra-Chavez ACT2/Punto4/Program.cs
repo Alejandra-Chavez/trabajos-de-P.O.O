6c5178b9ec4f97ee554b0a9750e84fba4f3ba98b
Alejandra-Chavez <alejandrachavezu9@gmail.com>
2026-04-10 10:45:32 -0300

    Add files via upload

---
+++ Alejandra-Chavez ACT2/Punto4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*4. Se cargan por teclado tres números distintos. Mostrar por pantalla el mayor de ellos.*/
+
+            int num1, num2, num3;
+
+            Console.Write("Ingrese el primer número: ");
+            string linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+
+            Console.Write("Ingrese el segundo número: ");
+            linea = Console.ReadLine();
+            num2 = int.Parse(linea);
+
+            Console.Write("Ingrese el tercer número: ");
+            linea = Console.ReadLine();
+            num3 = int.Parse(linea);
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine("El mayor es: " + num1);
+                }
+                else
+                {
+                    Console.WriteLine("El mayor es: " + num3);
+                }
+            }
+            else
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine("El mayor es: " + num2);
+                }
+                else
+                {
+                    Console.WriteLine("El mayor es: " + num3);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
