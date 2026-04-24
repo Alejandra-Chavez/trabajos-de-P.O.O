216508f1b6bc141c4e7d40c749e0f316d042a49a
Alejandra-Chavez <alejandrachavezu9@gmail.com>
2026-04-24 10:50:43 -0300

    Add files via upload

---
+++ Alejandra-Chavez ACT7/Alejandra-Chavez ACT7/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alejandra_Chavez_ACT7
+{
+    internal class Mayor
+    {
+        /*1. Desarrollar un programa que permita ingresar un vector de 8 elementos, e informe:
+El valor acumulado de todos los elementos del vector.
+El valor acumulado de los elementos del vector que sean mayores a 36.
+Cantidad de valores mayores a 50.
+             */
+
+        private int[] elementos;
+        string linea;
+
+        public void cargaNumeros()
+        {
+            elementos = new int[8];
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write("Ingresa un numero:");
+                linea = Console.ReadLine();
+                elementos[i] = int.Parse(linea);
+            }
+
+        }
+        public void sumaElementos()
+        {
+            int suma;
+            suma = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                suma = suma + elementos[i];
+            }
+            Console.WriteLine("la suma de los numeros ingresados es de:" + suma);
+        }
+        public void sumaNumMayores()
+        {
+            int suma;
+            suma = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                if (elementos[i] > 36)
+                {
+                    suma = suma + elementos[i];
+                }
+            }
+            Console.WriteLine("La suma de los numeros mayoresa a 36 es de :" + suma);
+        }
+        public void numMayor()
+        {
+            int mayor = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                if (elementos[i] > 50)
+                {
+                    mayor++;
+                }
+            }
+            Console.WriteLine("La cantidad de numeros mayores a 50 es de:" + mayor);
+        }
+
+        static void Main(string[] args)
+        {
+            Mayor M = new Mayor();
+            M.cargaNumeros();
+            M.sumaElementos();
+            M.sumaNumMayores();
+            M.numMayor();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Alejandra-Chavez ACT7/Punto2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    internal class sumaVectores
+    {
+        /*2. Realizar un programa que pida la carga de dos vectores numéricos enteros de 4
+elementos. Obtener la suma de los dos vectores, dicho resultado guardarlo en un
+tercer vector del mismo tamaño. Sumar componente a componente.*/
+
+        private int[] Vec1;
+        private int[] Vec2;
+        private int[] Vec3;
+        string linea;
+
+        public void IngresoDeDatos()
+        {
+            Vec1= new int[4];
+            Vec2 = new int[4];
+            Console.Write("Ingresa cuatro numeros para el primer vector =");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Ingresa un numero:");
+                linea = Console.ReadLine();
+                Vec1[i] = int.Parse(linea);
+            }
+            Console.Write("Ingresa cuatro numeros para el segundo vector =");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Ingresa un numero:");
+                linea = Console.ReadLine();
+                Vec2[i] = int.Parse(linea);
+            }
+
+        }
+        public void sumaEntreVectores()
+        {
+            int sum;
+            sum = 0;
+            for (int i = 0; i < 4; i++)
+            {
+               sum = Vec1[i] + Vec2[i];
+                Vec3 = new int[sum];
+                Console.WriteLine("La suma de cada componenete da:" + sum);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            sumaVectores s = new sumaVectores();
+            s.IngresoDeDatos();
+            s.sumaEntreVectores();
+
+            Console.ReadKey();
+        }
+    }
+}
