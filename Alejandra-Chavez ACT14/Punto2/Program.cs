2d87e694529906de818b59ea834dad74f3c8cf50
Alejandra-Chavez <alejandrachavezu9@gmail.com>
2026-06-04 20:10:12 -0300

    subo la actividad 14

---
+++ Alejandra-Chavez ACT14/Punto2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    /*2. Control de Vuelos
+Plantear una clase llamada Pasajero que tenga los atributos privados: Nombre, DNI y EquipajeKilos (peso del equipaje en kg). En su constructor, solicitar la carga de estos tres datos.
+Luego, confeccionar una clase llamada Vuelo que administre un vector de 4 objetos de la clase Pasajero. Su constructor debe inicializar el vector y solicitar la carga por teclado de cada
+uno de los 4 pasajeros.
+Agregar los siguientes métodos en la clase Vuelo:
+Un método que muestre un listado completo con los datos de todos los pasajeros del vuelo.
+Un método que calcule y muestre el peso total de equipaje que transporta el avión (la suma de todos los pesos).
+Un método que informe si algún pasajero excede el límite permitido de equipaje (mayor a 23 kg), imprimiendo su nombre y su DNI.
+*/
+    class pasajero
+    {
+        private string nombre;
+        private int DNI;
+        private float pesoEquipaje;
+
+        public pasajero()
+        {
+            Console.Write("Ingresa el nombre del pasajero: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingresa el DNI del pasajero: ");
+            string linea = Console.ReadLine();
+            DNI = int.Parse(linea);
+            Console.Write("Por ultimo, por favor ingrese el peso de su equipaje: ");
+            linea = Console.ReadLine();
+            pesoEquipaje = float.Parse(linea);
+        }
+        public string returnNombre()
+        {
+            return nombre;
+        }
+        public int returnDNI()
+        {
+            return DNI;
+        }
+        public float returnPesoEquipaje()
+        {
+            return pesoEquipaje;
+        }
+    }
+       class vuelo
+    {
+        private pasajero[] pasajeros;
+        public vuelo()
+        {
+            pasajeros = new pasajero[4];
+            Console.WriteLine("Complete la informacion de cada pasajero: ");
+            for (int i = 0; i < pasajeros.Length; i++)
+            {
+                Console.WriteLine("Pasajero" + (i + 1));
+            pasajeros[i] = new pasajero();
+
+        }
+        }
+        public void imprimir()
+        {
+            Console.WriteLine("Listado de los pasajeros: ");
+            for (int i = 0; i < pasajeros.Length; i++)
+            {
+
+                Console.WriteLine("Nombre: " + pasajeros[i].returnNombre());
+                Console.WriteLine(" DNI: " + pasajeros[i].returnDNI());
+                Console.WriteLine("Peso del Equipaje: " + pasajeros[i].returnPesoEquipaje());
+                Console.WriteLine("-----------------------------------");
+            }
+        }
+        public void pesoTotal()
+        {
+            float suma = 0;
+            for(int i = 0; i < pasajeros.Length; i++)
+            {
+                suma = suma + pasajeros[i].returnPesoEquipaje();
+            }
+            Console.WriteLine("El peso total del de equipajes en el avion es de " + suma + "Kg");
+        }
+        public void limitePeso()
+        {
+            for (int i = 0; i < pasajeros.Length; i++)
+            {
+                if (pasajeros[i].returnPesoEquipaje() > 23)
+                {
+                    Console.WriteLine("El pasajer@ " + pasajeros[i].returnNombre() + "con el DNI " + pasajeros[i].returnDNI() +"excede con el limite de peso");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            vuelo V = new vuelo();
+            V.imprimir();
+            V.pesoTotal();
+            V.limitePeso();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Alejandra-Chavez ACT14/Punto4/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto4
+{
+    /*4. Inventario de Sucursales 
+Plantear una clase llamada Articulo que tenga como atributos privados el NombreArticulo, el Precio y el StockActual. El constructor de la clase debe cargar estos atributos por teclado.
+Confeccionar otra clase llamada Sucursal que contenga un vector de 3 objetos de la clase Articulo. El constructor de Sucursal debe pedir el nombre o número de la sucursal y realizar la carga de los 3 artículos.
+Implementar en la clase Sucursal los siguientes métodos:
+Un método que imprima la lista de los artículos junto con su valor total en inventario (Precio * Stock).
+Un método que busque y muestre el nombre del artículo más caro del local.
+Un método que verifique e indique qué artículos de la sucursal requieren reposición urgente (aquellos cuyo stock sea menor o igual a 5 unidades).
+*/
+    class Articulo
+    {
+        private string nombreArticulo;
+        private double precio;
+        private int stockActual;
+
+        public Articulo()
+        {
+            Console.Write("Ingrese el nombre del articulo: ");
+            nombreArticulo = Console.ReadLine();
+
+            Console.Write("Ingrese el precio del articulo: ");
+            string linea = Console.ReadLine();
+            precio = double.Parse(linea);
+
+            Console.Write("Ingrese la cantidad en stock: ");
+            linea = Console.ReadLine();
+            stockActual = int.Parse(linea);
+        }
+
+        public string returnNombreProducto()
+        {
+            return nombreArticulo;
+        }
+
+        public double returnPrecio()
+        {
+            return precio;
+        }
+
+        public int returnStock()
+        {
+            return stockActual;
+        }
+    }
+
+    class Sucursal
+    {
+        private int numSucursal;
+        private Articulo[] articulos;
+
+        public Sucursal()
+        {
+            Console.Write("Ingrese el numero o identificador de la sucursal: ");
+            string linea = Console.ReadLine();
+            numSucursal = int.Parse(linea);
+
+            articulos = new Articulo[3];
+
+            Console.WriteLine("Carga de articulos:");
+
+            for (int i = 0; i < articulos.Length; i++)
+            {
+                Console.WriteLine("Articulo " + (i + 1));
+                articulos[i] = new Articulo();
+            }
+        }
+
+        public void imprimir()
+        {
+            Console.WriteLine("Datos de la sucursal N° " + numSucursal);
+
+            for (int i = 0; i < articulos.Length; i++)
+            {
+                double valorTotal = articulos[i].returnPrecio() * articulos[i].returnStock();
+
+                Console.WriteLine("Articulo: " + articulos[i].returnNombreProducto() + " | Valor total en inventario: $" + valorTotal);
+            }
+        }
+
+        public void articuloMayorPrecio()
+        {
+            int mayor = 0;
+
+            for (int i = 1; i < articulos.Length; i++)
+            {
+                if (articulos[i].returnPrecio() > articulos[mayor].returnPrecio())
+                {
+                    mayor = i;
+                }
+            }
+
+            Console.WriteLine("El articulo mas caro es: " + articulos[mayor].returnNombreProducto() + " con un precio de $" + articulos[mayor].returnPrecio());
+        }
+
+        public void menorCantidadArticulo()
+        {
+            Console.WriteLine("Articulos que requieren reposicion urgente:");
+
+            for (int i = 0; i < articulos.Length; i++)
+            {
+                if (articulos[i].returnStock() <= 5)
+                {
+                    Console.WriteLine(articulos[i].returnNombreProducto());
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Sucursal s = new Sucursal();
+
+            s.imprimir();
+            s.articuloMayorPrecio();
+            s.menorCantidadArticulo();
+
+            Console.ReadKey();
+        }
+    }
