87e150db7a120c772a19528f6ee0e8de5b4cc10c
Alejandra-Chavez <alejandrachavezu9@gmail.com>
2026-06-11 08:40:29 -0300

    subo la actividad 12

---
+++ Alejandra-Chavez ACT12/Punto 2/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto_2
+{
+        /*2. Gestión de Complejo de Cine
+Un cine tiene 4 salas con diferentes capacidades de espectadores (la Sala 1 tiene 10
+asientos, la Sala 2 tiene 15, la Sala 3 tiene 8 y la Sala 4 tiene 12).
+● Definir una matriz irregular de 4 filas para representar los asientos.
+● Métodos:
+1. Inicializar la matriz con los tamaños de las salas mencionadas (sin
+intervención del operador).
+2. Crear un método de "Venta de Entradas" que permita cargar la edad del
+espectador en un asiento específico (fila y columna).
+3. Imprimir el mapa de ocupación de las salas indicando la edad del espectador
+en cada asiento.
+4. Calcular cuántos menores de edad (menos de 18 años) hay en cada sala.
+5. Informar cuál es el promedio de edad de los espectadores de todo el
+complejo.*/
+
+class Cine
+    {
+        private int[][] salas;
+
+        public void Inicializar()
+        {
+            salas = new int[4][];
+
+            salas[0] = new int[10];
+            salas[1] = new int[15];
+            salas[2] = new int[8];
+            salas[3] = new int[12];
+        }
+
+        public void VentaEntrada()
+        {
+            int sala, asiento, edad;
+
+            Console.Write("Ingrese sala (1-4): ");
+            string linea = Console.ReadLine();
+            sala = int.Parse(linea);
+
+            Console.Write("Ingrese asiento: ");
+            linea = Console.ReadLine();
+            asiento = int.Parse(linea);
+
+            if (asiento < 1 || asiento > salas[sala - 1].Length)
+            {
+                Console.WriteLine("Asiento inválido");
+                return;
+            }
+
+            Console.Write("Ingrese edad: ");
+            linea = Console.ReadLine();
+            edad = int.Parse(linea);
+
+            salas[sala - 1][asiento - 1] = edad;
+        }
+
+        public void MostrarSalas()
+        {
+            for (int f = 0; f < salas.Length; f++)
+            {
+                Console.WriteLine("Sala " + (f + 1));
+
+                for (int c = 0; c < salas[f].Length; c++)
+                {
+                    Console.Write(salas[f][c] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public void MenoresPorSala()
+        {
+            for (int f = 0; f < salas.Length; f++)
+            {
+                int menores = 0;
+
+                for (int c = 0; c < salas[f].Length; c++)
+                {
+                    if (salas[f][c] > 0 && salas[f][c] < 18)
+                    {
+                        menores++;
+                    }
+                }
+
+                Console.WriteLine("Sala " + (f + 1) +": " + menores + " menores");
+            }
+        }
+
+        public void PromedioEdad()
+        {
+            int suma = 0;
+            int cantidad = 0;
+
+            for (int f = 0; f < salas.Length; f++)
+            {
+                for (int c = 0; c < salas[f].Length; c++)
+                {
+                    if (salas[f][c] > 0)
+                    {
+                        suma += salas[f][c];
+                        cantidad++;
+                    }
+                }
+            }
+
+            if (cantidad > 0)
+            {
+                double promedio = (double)suma / cantidad;
+                Console.WriteLine("Promedio de edad: " + promedio);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Cine cine = new Cine();
+
+            cine.Inicializar();
+
+            cine.VentaEntrada();
+            cine.VentaEntrada();
+            cine.VentaEntrada();
+
+            cine.MostrarSalas();
+            cine.MenoresPorSala();
+            cine.PromedioEdad();
+
+            Console.ReadKey();
+        }
+    
+}
+}
---
+++ Alejandra-Chavez ACT12/Punto1/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    internal class sistemLogistica
+    {
+        /*1. Sistema de Logística: Envíos por Sucursal
+Una empresa de correo tiene 3 sucursales principales. Cada sucursal procesa una
+cantidad diferente de paquetes por día dependiendo de su demanda.
+● Definir un vector de tipo string para los nombres de las 3 sucursales.
+● Definir una matriz irregular donde cada fila sea una sucursal y cada columna
+represente el peso (en kg) de cada paquete enviado.
+● Métodos:
+1. Cargar los nombres de las sucursales y, para cada una, preguntar cuántos
+paquetes se enviaron hoy para definir el tamaño de su fila.
+2. Cargar el peso de cada paquete.
+3. Imprimir el peso de todos los paquetes organizados por sucursal.
+4. Calcular e informar el peso total despachado por cada sucursal.
+5. Informar cuál es el paquete más pesado de toda la empresa y a qué sucursal
+pertenece.*/
+        private string[] sucursal;
+        private int[][] pesoPaquete;
+        string linea;
+        int cantidad;
+
+        public void cargarDatos()
+        {
+            sucursal = new string[3];
+
+            pesoPaquete = new int[3][];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Ingresa el nombre de la " + (i + 1) + "° Sucursal: ");
+                sucursal[i] = Console.ReadLine();
+
+                Console.Write("¿Cuántos paquetes envió hoy?: ");
+                linea = Console.ReadLine();
+
+                cantidad = int.Parse(linea);
+
+                pesoPaquete[i] = new int[cantidad];
+
+                for (int j = 0; j < cantidad; j++)
+                {
+                    Console.Write("Ingresa el peso del paquete: ");
+                    linea = Console.ReadLine();
+
+                    pesoPaquete[i][j] = int.Parse(linea);
+                }
+
+                Console.WriteLine();
+            }
+        }
+        public void totalPesoPaquetes()
+        {
+            int suma;
+
+            Console.WriteLine("peso total de los paquetes");
+
+            for (int i = 0; i < 3; i++)
+            {
+                suma = 0;
+
+                for (int j = 0; j < pesoPaquete[i].Length; j++)
+                {
+                    suma += pesoPaquete[i][j];
+                }
+
+                Console.WriteLine("Sucursal " + sucursal[i] + ": " + suma + " kg");
+            }
+            Console.WriteLine();
+        }
+        public void mayorPeso()
+        {
+            int mayor = pesoPaquete[0][0];
+            string sucursalMayor = sucursal[0];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < pesoPaquete[i].Length; j++)
+                {
+                    if (pesoPaquete[i][j] > mayor)
+                    {
+                        mayor = pesoPaquete[i][j];
+                        sucursalMayor = sucursal[i];
+                    }
+                }
+            }
+
+            Console.WriteLine( "El paquete más pesado es de " + mayor + " kg y pertenece a " + sucursalMayor);
+
+            Console.WriteLine();
+        }
+        public void imprimirInformacion()
+        {
+            Console.WriteLine("Lista de paquetes:");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Sucursal: " + sucursal[i]);
+
+                for (int j = 0; j < pesoPaquete[i].Length; j++)
+                {
+                    Console.WriteLine("Paquete " + (j + 1) + ": " + pesoPaquete[i][j] + " kg");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            sistemLogistica L = new sistemLogistica();
+            L.cargarDatos();
+            L.totalPesoPaquetes();
+            L.mayorPeso();
+            L.imprimirInformacion();
+
+            Console.ReadKey();
+        }
+    }
+}
